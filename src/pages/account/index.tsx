bc0612b8e5ed71946fbf2112c2256642072203a6
Zachary Tobey <ztobey@umass.edu>
2025-11-05 19:09:53 -0500

    Edited account.module.scss and index.tsx, added a default profile picture PNG in public/. Created a scaffolding of the account page for functionality implementation later.

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,11 +1,41 @@
 
 import React from 'react';
+import styles from './account.module.scss';
 
 function AccountPage() {
   return (
-    <div>
-      <h1>Welcome to the Account Page!</h1>
-      <p>You can access your account here.</p>
+    <div className={styles.page}>
+      <div className={styles.accountNAV}>
+        
+        <div className={styles.navLeft}>
+          <h2>Account Details</h2>
+        </div>
+
+        <div className={styles.navRight}>
+          <button className={styles.navButton}>Edit Profile</button>
+          <button className={styles.navButton}>Settings</button>
+          <button className={styles.navButton}>Help</button>
+        </div>
+      </div>
+
+      <div className={styles.profileContainer}>
+        <div className={styles.profileArea}>
+          <img src="/Default-pfp.png" alt="" className={styles.profilePic} />
+          <div className={styles.username}>
+            Username goes here.
+          </div>
+        </div>
+
+        <div className={styles.contentArea}>
+          <h3>Account Overview</h3>
+
+          <p>Here's where you can see your current schedule.</p>
+          <button className={styles.contentButton}>Your Schedule</button>
+
+          <p>Here's where you can see your previous routes.</p>
+          <button className={styles.contentButton}>Your Routes</button>
+        </div>
+      </div>
     </div>
   );
 }
