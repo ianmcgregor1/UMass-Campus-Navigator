de19977c5679bf4664e125b73bf7da8db6e3dd50
ztobey1 <ztobey@umass.edu>
2025-12-08 21:02:41 -0500

    finished up the location.test.js file

--- tests/routes/locations.test.js
+++ tests/routes/locations.test.js
@@ -1,5 +1,25 @@
+const request = require("supertest");
+//mock the current database.
+jest.mock("../../server/config/db");
+const db = require("../../server/config/db");
+const app = require("../../server/server");
+
 describe('Locations API', () => {
-  it('placeholder', () => {
-    expect(true).toBe(true);
-  });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("should retrieve all location", async() => {
+        const mockLocations = [
+            {id:1, name:"Lederle", location: {lat: 123, lng: 234}},
+            {id:2, name:"ILC", location: {lat: 345, lng: 456}}
+        ];
+        db.query.mockImplementation((query, callback) => {
+            callback(null, mockLocations);
+        })
+        const response = await request(app).get("/api/locations");
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(response.body)).toEqual(true);
+        expect(response.body[0].location).toEqual({lat: 123, lng: 234});
+    });
 });
